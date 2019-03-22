9241e0a3797c3d85c607f922b3b766ebfe8d8f80
Jamebadboy <59304070@g.chula.edu>
2019-03-22 16:47:29 +0700

    Do something magical

--- loadbalancer/app.js
+++ loadbalancer/app.js
@@ -9,7 +9,7 @@ var cors = require('cors');
 var app = express();
 
 var io = require('socket.io')();
-io.on('connection', function (client) {
+io.on('connection',function (client) {
   console.log('a user connected.');
 });
 io.listen(3001);
@@ -21,30 +21,30 @@ var indexController = require('./controllers/index');
 app.use(cors());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
   next();
 });
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public','favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/', indexController);
+app.use('/',indexController);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next){
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err,req,res,next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- loadbalancer/controllers/index.js
+++ loadbalancer/controllers/index.js
@@ -24,29 +24,29 @@ getPaths = [
 ];
 
 postPaths.map(path => {
-  router.post(path, function (req, res, next) {
+  router.post(path, function (req,res,next) {
     // ACTIVE PRIMARY BACKEND
-    axios.post(ip.primaryBackend + path, req.body)
+    axios.post(ip.primaryBackend + path,req.body)
       .then(function (response) {
         if (activeBackend === 2) {
           console.log("primary server is back and taking over");
           activeBackend = 1;
         }
-        if (path === '/sendMessage') io.emit('chat', response.data);
+        if (path === '/sendMessage') io.emit('chat',response.data);
         console.log('From: primary');
         res.send(response.data);
       })
       .catch(function (err) {
 
         // ACTIVE SECONDARY BACKEND
-        axios.post(ip.secondaryBackend + path, req.body)
+        axios.post(ip.secondaryBackend + path,req.body)
           .then(function (response) {
             if (activeBackend === 1) {
               console.log("primary server is not working");
               console.log("secondary server is taking over");
               activeBackend = 2;
             }
-            if (path === '/sendMessage') io.emit('chat', response.data);
+            if (path === '/sendMessage') io.emit('chat',response.data);
             console.log('From: secondary');
             res.send(response.data);
           })
@@ -59,9 +59,9 @@ postPaths.map(path => {
 });
 
 getPaths.map(path => {
-  router.get(path, function (req, res, next) {
+  router.get(path, function (req,res,next) {
     // ACTIVE PRIMARY BACKEND
-    axios.get(ip.primaryBackend + path, { params: req.query })
+    axios.get(ip.primaryBackend + path,{ params: req.query })
       .then(function (response) {
         if (activeBackend === 2) {
           console.log("primary server is back and taking over");
@@ -73,7 +73,7 @@ getPaths.map(path => {
       .catch(function (err) {
 
         // ACTIVE SECONDARY BACKEND
-        axios.get(ip.secondaryBackend + path, { params: req.query })
+        axios.get(ip.secondaryBackend + path,{ params: req.query })
           .then(function (response) {
             if (activeBackend === 1) {
               console.log("primary server is not working");
